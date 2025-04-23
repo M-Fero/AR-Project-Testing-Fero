da8c3eff929e0d195084e19e68ac59f9c41ed180
Omar Khaled <fero.nitro@gmail.com>
2025-04-23 18:16:17 +0200

    Update Character.cs

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Character : ARInteractableObject
 {
+    [SerializeField] private Renderer renderer;
     private Animator animator;  
     private void OnEnable()
     {
@@ -15,8 +16,14 @@ public class Character : ARInteractableObject
             case State.Idle:
                 animator.SetTrigger("GoToIdle");
                 break;
+                if(renderer == null) return;
+                renderer.materials[0].EnableKeyword("_EMISSION");
+                renderer.materials[0].SetColor("_EmissionColor", new Color(0.5f, 0.5f,0.5f,0.1f));
             case State.Active:
                 animator.SetTrigger("StartInteraction");
+                if (renderer == null) return;
+                renderer.materials[0].EnableKeyword("_EMISSION");
+                renderer.materials[0].SetColor("_EmissionColor", new Color(0, 0, 0, 0));
                 break;
         }
     }
